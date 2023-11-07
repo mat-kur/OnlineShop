787f5d6f014ecb5bec939b70341705a405e61f31
unknown <11elka99@gmail.com>
2023-11-07 20:49:41 +0100

    added new idea

--- frontend/src/pages/Admin/Products/HotDeals/HotDeals.tsx
+++ frontend/src/pages/Admin/Products/HotDeals/HotDeals.tsx
@@ -2,6 +2,9 @@ import "./HotDeals.css";
 
 import fifa from "../../../Home/ShopItems/i-fifa-23-gra-pc.png"
 import React from "react";
+import {eventWrapper} from "@testing-library/user-event/dist/utils";
+import {AdminNav} from "../../../../components/Admin/AdminNav/AdminNav";
+
 export const HotDeals = () => {
 
     const products = [
@@ -12,23 +15,43 @@ export const HotDeals = () => {
         {id: 5, title: 'ELEX 2', platform: 'PC', price: '20$'},
         {id: 6, title: 'ELEX 2', platform: 'PC', price: '20$'},
         {id: 7, title: 'ELEX 2', platform: 'PC', price: '20$'},
-        // Dodaj tutaj inne produkty...
     ];
 
     return (
         <>
-            <h3 className="hot-deal-title">Hot deal's</h3>
-            <div className="hot-deals-wrapper">
-                {products && products.map (prod =>
-                    <div className="hot-deal">
-                        <p className="hot-deals-title">{prod.title}</p>
-                        <img src={fifa} alt="" className="hot-deals-img"/>
-                        <p className="hot-deals-platform">Platfrom: <span>{prod.platform}</span></p>
-                        <p className="hot-deals-platform">Price: <span>{prod.price}zł</span></p>
-                        <button className="hot-deal-del">DELETE</button>
+            <div className="deals-container">
+                <h3 className="hot-deal-title">Hot deal's</h3>
+                <div className="hot-deals-wrapper">
+                    {products && products.map(prod =>
+                        <div className="hot-deal">
+                            <p className="hot-deals-title">{prod.title}</p>
+                            <img src={fifa} alt="" className="hot-deals-img"/>
+                            <p className="hot-deals-platform">Platfrom: <span>{prod.platform}</span></p>
+                            <p className="hot-deals-platform">Price: <span>{prod.price}zł</span></p>
+                            <button className="hot-deal-del">DELETE</button>
+                        </div>
+                    )}
+                </div>
+                <div className="all-products">
+                    <h3 className="add-hotdeals-title">Add items to Hot-Deal's</h3>
+                    <div className="prod-searchbar">
+                        <input type="text"/>
+                        <i className="deals-search fa-solid fa-magnifying-glass"></i>
                     </div>
-                )}
+                    <div className="wrapper-deals">
+                        {products && products.map(prod =>
+                            <div className="hot-deals-items">
+                                <p className="items-title">FIFA 2023</p>
+                                <img src={fifa} alt="" className="hot-deals-img"/>
+                                <p className="hot-deals-platform">Platfrom: <span>{prod.platform}</span></p>
+                                <p className="hot-deals-platform">Price: <span>{prod.price}zł</span></p>
+                                <button className="hot-deal-add">ADD</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
+            <AdminNav/>
         </>
     );
 };
